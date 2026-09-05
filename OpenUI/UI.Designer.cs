a3d6ca8894df457cf0539940bf4fecd01190432a
Chuyanglife <yaohong96@gmail.com>
2026-09-05 23:55:14 +0800

    ok

--- OpenUI/UI.Designer.cs
+++ OpenUI/UI.Designer.cs
@@ -1,46 +1,5 @@
-﻿namespace OpenUI
+namespace OpenUI
 {
-    partial class UI
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // UI
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1484, 804);
-            this.Name = "UI";
-            this.Text = "UI";
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
-    }
+    // UI controls and initialization are kept in UI.cs.
+    partial class UI { }
 }
