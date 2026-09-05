8834cc3315e45454b839efc97e436ae568576a43
Chuyanglife <yaohong96@gmail.com>
2026-09-05 18:31:50 +0800

    ok

--- OpenUI/UI.Designer.cs
+++ OpenUI/UI.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1484, 804);
+            this.Name = "UI";
             this.Text = "UI";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
