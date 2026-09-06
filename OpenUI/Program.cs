d3e74e9d917a9df134d62a593052c843da9649ba
Chuyanglife <yaohong96@gmail.com>
2026-09-06 22:18:17 +0800

    ok

--- OpenUI/Program.cs
+++ OpenUI/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OpenUI
+namespace opusViewerPro
 {
     internal static class Program
     {
--- OpenUI/Properties/Resources.Designer.cs
+++ OpenUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OpenUI.Properties
+namespace opusViewerPro.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace OpenUI.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("opusViewerPro.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
