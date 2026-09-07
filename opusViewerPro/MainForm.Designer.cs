106b420ad73bcf9557d54d3f55ef314bceef881f
Anthony Wong <40868137+anthonnyy-ny@users.noreply.github.com>
2026-09-07 09:45:46 +0800

    k

--- opusViewerPro/MainForm.Designer.cs
+++ opusViewerPro/MainForm.Designer.cs
@@ -17,20 +17,123 @@
 
         private void InitializeComponent()
         {
+            this.brandLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.modelLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.versionLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.loginLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox3 = new Guna.UI2.WinForms.Guna2ControlBox();
             this.SuspendLayout();
-            //
+            // 
+            // brandLabel
+            // 
+            this.brandLabel.BackColor = System.Drawing.Color.Transparent;
+            this.brandLabel.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Bold);
+            this.brandLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(28)))), ((int)(((byte)(38)))));
+            this.brandLabel.Location = new System.Drawing.Point(14, 10);
+            this.brandLabel.Margin = new System.Windows.Forms.Padding(0, 1, 10, 0);
+            this.brandLabel.Name = "brandLabel";
+            this.brandLabel.Size = new System.Drawing.Size(92, 37);
+            this.brandLabel.TabIndex = 1;
+            this.brandLabel.Text = "oToPUs";
+            // 
+            // modelLabel
+            // 
+            this.modelLabel.BackColor = System.Drawing.Color.Transparent;
+            this.modelLabel.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.modelLabel.Location = new System.Drawing.Point(126, 18);
+            this.modelLabel.Margin = new System.Windows.Forms.Padding(10, 9, 12, 0);
+            this.modelLabel.Name = "modelLabel";
+            this.modelLabel.Size = new System.Drawing.Size(69, 19);
+            this.modelLabel.TabIndex = 2;
+            this.modelLabel.Text = "Nova AA-2";
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.versionLabel.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Bold);
+            this.versionLabel.Location = new System.Drawing.Point(233, 23);
+            this.versionLabel.Margin = new System.Windows.Forms.Padding(0, 4, 0, 0);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(153, 14);
+            this.versionLabel.TabIndex = 3;
+            this.versionLabel.Text = "Ver. 1.1.0.7   Release Date: 2026/8/31";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.BackColor = System.Drawing.Color.Transparent;
+            this.loginLabel.Font = new System.Drawing.Font("Segoe UI", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.loginLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.loginLabel.Location = new System.Drawing.Point(409, 19);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(191, 28);
+            this.loginLabel.TabIndex = 4;
+            this.loginLabel.Text = "Login Level : Operator<br>License expiration date : 12/31/2026";
+            this.loginLabel.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(914, 10);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 5;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(812, 10);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 6;
+            // 
+            // guna2ControlBox3
+            // 
+            this.guna2ControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.guna2ControlBox3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox3.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox3.Location = new System.Drawing.Point(863, 10);
+            this.guna2ControlBox3.Name = "guna2ControlBox3";
+            this.guna2ControlBox3.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox3.TabIndex = 7;
+            // 
             // MainForm
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 35F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(971, 518);
+            this.Controls.Add(this.guna2ControlBox3);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.modelLabel);
+            this.Controls.Add(this.brandLabel);
             this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "opusViewerPro";
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel brandLabel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel modelLabel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel versionLabel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel loginLabel;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox3;
     }
 }
